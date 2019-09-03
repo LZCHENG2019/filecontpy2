547e6a4cda156da93f32d54703ceee56b7a22f74
LZCHENG2019 <46548306+LZCHENG2019@users.noreply.github.com>
2019-09-03 17:05:51 +0800

    20190830:此版本已重新整理了tar包和xz包的截取分支名方式，已调试； 20190902：更新 20190903:此版本阈值共有三个：1、文件数量差异阈值；2、文件大小差异阈值；3、文件内容差异阈值。

--- filecontpy2.py
+++ filecontpy2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*
+#20190902存
 import hashlib,os,commands,sys
 #存储大包（tar）模板，key为模板的分支名，value为模板的路径
 def template_tar(branch):
@@ -35,54 +36,51 @@ def template_xz(branch):
                 }
     return dictsmall[branch]
 def compardirs(path1,path2,iteration=1):#对比文件夹内容
-    # file1m,file2m,diff_temp,diff_tested,file_tp,file_cont,file_size = 0,0,[],[],0,0,0
-    file1m, file2m,file_tp, file_cont, file_size = 0, 0, 0, 0, 0
-    difftp = 1                                      #此阈值表示文件类型差异数量差距(本次测试无这种情况)
-    diffcont = 1                                    #此阈值表示文件内容差异数量差距（大小相同，类型不同的情况，本次测试无这种情况）
-    diffsize = 1                                    #此阈值表示文件大小差异数量差距(出现该情况)
-    diff_files = 1                                  #此阈值表示文件缺少/增多数量差距（本次测试无该情况）
-    finresult = True
+    result = True
+
+    path_num_value = 1                  #文件数量差异阈值
+    path_size_value = 1                 #文件大小差异阈值
+    path_diff_value = 1                 #文件内容差异阈值
+
+    path1_num = 0
+    path2_num = 0
+    path1_size = 0
+    path2_size = 0
+
     files1_list = os.listdir(path1)                 #列出文件夹path下的文件列表
     files2_list = os.listdir(path2)
-    files1_list.sort()                              #将files_list列表进行排序
-    files2_list.sort()
-    file1m = len(files1_list)
-    file2m = len(files2_list)
-    for i in range(len(files1_list)):
-        name_temp = files1_list[i]
-        name_tested = files2_list[i]
-        file_temp = os.path.join(path1, name_temp)
-        file_tested = os.path.join(path2, name_tested)
-        # if os.path.isfile(file_temp) and os.path.isfile(file_tested):
-        result,filetp,filecont,filesize = comparelement(file_temp,file_tested)
-        # if result == True:                      #如果对比结果一致则返回True
-        #     finresult = True
-        if result == False and iteration == 1:
-            dir_temp = un_compress(file_temp)
-            dir_tested = un_compress(file_tested)
-            if os.path.exists(dir_temp) and os.path.exists(dir_tested):
-                result2 = compardirs(dir_temp,dir_tested,2)
-        if result2 == False:
-            finresult = False
-        if filetp == False:
-            file_tp += 1
-        if filecont == False:
-            file_cont += 1
-        if filesize == False:
-            file_size += 1
-    if abs(file1m-file2m) >= diff_files:
-        print ('文件夹%s与模板文件个数差异%s个' %(os.path.basename(path2),abs(file1m-file2m)))
-        finresult = False
-    if file_tp >= difftp:
-        print ('文件夹%s与模板件类型差异%s个' %(os.path.basename(path2),ile_tp))
-        finresult = False
-    if file_cont >= diffcont:
-        print ('文件夹%s与模板文件内容差异%s个' %(os.path.basename(path2),file_cont))
-        finresult = False
-    if file_size >= diffsize:
-        print ('文件夹%s与模板文件大小差异%s个' %(os.path.basename(path2),file_size))
-        finresult = False
-    return finresult     #,diff_temp,diff_tested
+
+    for i in range(len(files1_list)):               #解压path1内的全部压缩文件
+        file = path1+'/'+files1_list[i]
+        if os.path.isfile(file):
+            file_path = un_compress(file)
+    for i in range(len(files2_list)):
+        file =path2+'/'+files2_list[i]
+        if os.path.isfile(file):
+            file_path = un_compress(file)
+    try:
+        commands("\\rm %s/*.tar*" % path1)
+        commands("\\rm %s/*.tar*" % path2)
+    except:
+        pass
+
+    path1_num = int(commands.getstatusoutput("ls %s -lR | grep '^-' | wc -l" % path1)[1])       #计算path1文件夹内所有文件数（包含子文件）
+    path2_num = int(commands.getstatusoutput("ls %s -lR | grep '^-' | wc -l" % path2)[1])
+    path1_size = int(commands.getstatusoutput("du %s -s -k| awk '{print $1}'" % path1)[1])        #计算path1文件夹所占大小
+    path2_size = int(commands.getstatusoutput("du %s -s -k| awk '{print $1}'" % path2)[1])
+
+    a = commands.getstatusoutput("diff -rqH %s %s 2>/dev/null | wc -l" %(path1,path2))[1]
+    path_diff = int(a)         #计算path1与path2里面各级文件差异的数量
+    if abs(path1_size-path2_size) >= path_size_value:
+        result =  False
+        print ('文件大小差异%skb' %abs(path1_size-path2_size))
+    if abs(path1_num-path2_num) >= path_num_value:
+        result = False
+        print ('文件数量差异%s个' % abs(path1_num - path2_num))
+    if path_diff >= path_diff_value:
+        result = False
+        print ('文件内容差异%s个' % path_diff)
+    return result
 def find_template(file):
     filename = os.path.basename(file)
     if fileguess(file) == 'POSIX tar archive (GNU)':
@@ -133,81 +131,51 @@ def getMd5(file1):#获取MD5值
         m.update(str)
         return m.hexdigest()
 def comparelement(file1,file2):#对比文件
-    filetp = ''
-    filecont = ''
-    filesize = ''
-    sizevalue = 1 #此处用来定义文件大小阈值，单位是字节（B），可设置
-    result = True
+    result = False
     if os.path.isfile(file1) and os.path.isfile(file2):
         file_temp_typ = fileguess(file1)
         file_tested_typ = fileguess(file2)
         if file_temp_typ == file_tested_typ:                #先判断文件类型，若相同则继续对比，若不同则直接退出对比
             size_temp = os.path.getsize(file1)    #/ 1048576      #filesize(file1)
             size_tested = os.path.getsize(file2)  #/ 1048576    #filesize(file2)
-            size_diff = abs(size_temp-size_tested)
+            # size_diff = abs(size_temp-size_tested)
             if size_temp == size_tested:                    #先判断文件尺寸，若相同则对比MD5值
                 file1md5 = getMd5(file1)
                 file2md5 = getMd5(file2)
                 if file1md5 == file2md5:                    #两个文件大小、内容均无差异
                     result = True
+                    return result
                     # return result,filetp,filecont,filesize
                 else:                                       #两个文件大小相同，内容不同
                     # print ('文件大小差异为 %s bit' %abs(file1md5-file2md5))
-                    result = False
-                    filecont = False
-                    # return result,filetp,filecont,filesize
+                    file_temp_path = un_compress(file1)
+                    file_tested_path = un_compress(file2)
+                    # os.system("\\rm *.tar*")
+                    result = compardirs(file_temp_path,file_tested_path)
 
-            elif size_diff >= sizevalue:   #允许文件大小有差异，但是若大于某一阈值，则输出错误
-                # print ('%s与%s相差%sByte' %(os.path.basename(file1),os.path.basename(file2),abs(size_temp-size_tested)))
-                print ('filesize-|temp-tested|:%s/byte' %size_diff)
-                result = False
-                filesize = False
-                # return result,filetp,filecont,filesize
+            else:   #允许文件大小有差异，但是若大于某一阈值，则输出错误
+                file_temp_path = un_compress(file1)
+                file_tested_path = un_compress(file2)
+                # os.system("\\rm *.tar*")
+                result = compardirs(file_temp_path, file_tested_path)
+
         else:                                               #两个文件类型不同不进行对比
-            filetp = False
+            # filetp = False
             result = False
-    elif os.path.isdir(file1) and os.path.isdir(file2):
-        size_temp = int(commands.getstatusoutput("du --max-depth=0 -b %s | awk '{print $1}'" %file1)[1])
-        size_tested = int(commands.getstatusoutput("du --max-depth=0 -b %s | awk '{print $1}'" % file2)[1])
-        size_diff = abs(size_temp - size_tested)
-        if size_diff >= sizevalue:
-            print ('dirsize-|temp-tested|:%s/byte' %size_diff)
-            result = False
-            filesize = False
-    return result,filetp,filecont,filesize
+    # if os.path.isdir(file_temp_path) and os.path.isdir(file_tested_path):
+    #     os.system('\\rm -r %s %s' % (file_temp_path, file_tested_path))
+    return result
 def Main():
-    # Result = ''
     file_tested = sys.argv[1]                           #运行python时传入参数
     # filename = os.path.basename(File)
     class Networkerror(RuntimeError):
         def __init__(self, arg):
             self.args = arg
     try:
-        # if fileguess(File) == 'POSIX tar archive (GNU)':
-        #     #判断文件类型是否为Tar包
-        #     branch = commands.getstatusoutput(" echo %s | cut -d '-' -f 4- | sed 's/-2-1.*//' | rev | cut -d '_' -f 5- | rev" % filename)[1]
-        #     file_template = template_tar(branch)
-        #     print ('模板%s内:' % os.path.basename(file_template))
-        #     #file_template为对比模板文件
-        # elif fileguess(File) == 'XZ compressed data':
-        #     branch = commands.getstatusoutput(" echo %s | cut -d '-' -f 4- | sed 's/-2-1.*//' | rev | cut -d '_' -f 4- | rev" % filename)[1]
-        #     file_template = template_xz(branch)
-        #     print ('模板%s内:' % os.path.basename(file_template))
         file_template = find_template(file_tested)
         if os.path.isfile(file_template) and os.path.isfile(file_tested):
-            result, filetp, filecont, filesize = comparelement(file_template, file_tested)
-            # print result
-            if filetp == False:
-                Result = False
-            elif result == False:
-                path_temp = un_compress(file_template)
-                path_tested = un_compress(file_tested)
-                Result = compardirs(path_temp, path_tested)
-        # return Result
-        # output = result_main(file_template,File)
-        print Result
-        if os.path.isdir(path_temp) and os.path.isdir(path_temp):
-            os.system('\\rm -r %s %s'%(path_temp, path_tested))
+            result = comparelement(file_template, file_tested)
+        print result
     except KeyError,e:
         print '输入文件不存在或模板不存在'
 if __name__=='__main__':
